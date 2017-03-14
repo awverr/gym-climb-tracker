b88008edde4b02f9c50d06ccaa4ad67c607b259e
Aubry Verret <aubry@awverret.com>
2017-03-14 15:48:14 -0700

    Added saveClimb() methods for TOP_ROPE and BOULDER climbs.

--- app/src/main/java/com/awverret/gymclimbtracker/store/CloudStore.java
+++ app/src/main/java/com/awverret/gymclimbtracker/store/CloudStore.java
@@ -1,13 +1,17 @@
 package com.awverret.gymclimbtracker.store;
 
+import com.awverret.gymclimbtracker.model.BoulderClimb;
 import com.awverret.gymclimbtracker.model.Climb;
 import com.awverret.gymclimbtracker.model.LeadClimb;
+import com.awverret.gymclimbtracker.model.TopRopeClimb;
 
 /**
- * Created by aubry on 3/1/17.
+ * Created by aubry on 3/1/17. Storage interface.
  */
 
 public interface CloudStore {
 
-    void saveLeadClimb(LeadClimb climb);
+    void saveClimb(LeadClimb climb);
+    void saveClimb(TopRopeClimb climb);
+    void saveClimb(BoulderClimb climb);
 }
