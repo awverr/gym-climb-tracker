cda972786abac984cbf5709d5bd5f510a78f818c
Aubry Verret <aubry@awverret.com>
2017-03-15 16:10:38 -0700

    Added color, wall, name, and setter plus getter methods and constructor to Route Class.

--- app/src/main/java/com/awverret/gymclimbtracker/model/Route.java
+++ app/src/main/java/com/awverret/gymclimbtracker/model/Route.java
@@ -2,6 +2,8 @@ package com.awverret.gymclimbtracker.model;
 
 import java.util.Date;
 
+import static java.util.UUID.randomUUID;
+
 /**
  * Created by aubry on 2/24/17.
  */
@@ -9,14 +11,42 @@ import java.util.Date;
 public class Route {
 
     String id;
-    Color color;
-   // Wall wall;
-    String routeName;
-   // Setter setter;
+    RouteColor color;
+    RouteWall wall;
+    String name;
+    RouteSetter setter;
     Date setDate;
 
     public Route() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public Route(String name, RouteSetter setter, RouteColor color, RouteWall wall){
+        id = randomUUID().toString();
+        this.name = name;
+        this.setter = setter;
+        this.color = color;
+        this.wall = wall;
+    }
+
+    public RouteSetter getSetter() {
+        return setter;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public RouteColor getColor() {
+        return color;
+    }
+
+    public RouteWall getWall() {
+        return wall;
+    }
+
+    public String getId() {
+        return id;
     }
 
 
