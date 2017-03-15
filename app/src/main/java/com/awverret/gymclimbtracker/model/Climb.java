90d78228be2794de3c12d389223c7fb17d3db361
Aubry Verret <aubry@awverret.com>
2017-03-14 17:18:36 -0700

    Added id, color, route name, setter, and date fields to Climb class.

--- app/src/main/java/com/awverret/gymclimbtracker/model/Climb.java
+++ app/src/main/java/com/awverret/gymclimbtracker/model/Climb.java
@@ -1,12 +1,23 @@
 package com.awverret.gymclimbtracker.model;
 
+import java.util.Date;
+
 /**
  * Created by aubry on 2/24/17.
  */
 
 public class Climb {
 
+    String id;
+    Color color;
+   // Wall wall;
+    String routeName;
+   // Setter setter;
+    Date setDate;
+
     public Climb() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
+
+
 }
