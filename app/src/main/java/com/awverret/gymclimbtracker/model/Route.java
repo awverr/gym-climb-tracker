fe8654bbf000dac1303d1f7c6b5c702287f0c934
Aubry Verret <aubry@awverret.com>
2017-03-17 14:44:32 -0700

    Moved all route properties except for ID to individual route type classes.

--- app/src/main/java/com/awverret/gymclimbtracker/model/Route.java
+++ app/src/main/java/com/awverret/gymclimbtracker/model/Route.java
@@ -11,11 +11,7 @@ import static java.util.UUID.randomUUID;
 public class Route {
 
     String id;
-    RouteColor color;
-    RouteWall wall;
-    String name;
-    RouteSetter setter;
-    Date setDate;
+
 
     public Route() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
@@ -23,30 +19,10 @@ public class Route {
 
     public Route(String name, RouteSetter setter, RouteColor color, RouteWall wall, Date setDate){
         id = randomUUID().toString();
-        this.name = name;
-        this.setter = setter;
-        this.color = color;
-        this.wall = wall;
-        this.setDate = setDate;
-    }
 
-    public RouteSetter getSetter() {
-        return setter;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public RouteColor getColor() {
-        return color;
-    }
-
-    public RouteWall getWall() {
-        return wall;
     }
 
     public String getId() {return id;}
 
-    public Date getSetDate() {return setDate;}
+
 }
