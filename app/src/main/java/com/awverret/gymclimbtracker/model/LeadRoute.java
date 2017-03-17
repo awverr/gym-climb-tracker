7d8eb32e4a4cd4d85e1d299cd65a3149da967a64
Aubry Verret <aubry@awverret.com>
2017-03-17 14:49:40 -0700

    Added route properties that were previously contained in Route class: RouteSetter, RouteColor, RouteWall, SetDate, RouteName.

--- app/src/main/java/com/awverret/gymclimbtracker/model/LeadRoute.java
+++ app/src/main/java/com/awverret/gymclimbtracker/model/LeadRoute.java
@@ -1,5 +1,7 @@
 package com.awverret.gymclimbtracker.model;
 
+import java.util.Date;
+
 import static java.util.UUID.randomUUID;
 
 /**
@@ -10,13 +12,23 @@ public class LeadRoute extends Route {
 
 
     RopeGrade ropeGrade;
+    RouteColor color;
+    RouteWall wall;
+    String name;
+    RouteSetter setter;
+    Date setDate;
 
     public LeadRoute() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
-    public LeadRoute(RopeGrade ropeGrade) {
+    public LeadRoute(RopeGrade ropeGrade, String name, RouteSetter setter, RouteColor color, RouteWall wall, Date setDate) {
         this.ropeGrade = ropeGrade;
+        this.name = name;
+        this.setter = setter;
+        this.color = color;
+        this.wall = wall;
+        this.setDate = setDate;
     }
 
     public String getId() {
@@ -26,4 +38,22 @@ public class LeadRoute extends Route {
     public RopeGrade getRopeGrade() {
         return ropeGrade;
     }
+
+    public RouteSetter getSetter() {
+        return setter;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public RouteColor getColor() {
+        return color;
+    }
+
+    public RouteWall getWall() {
+        return wall;
+    }
+
+    public Date getSetDate() {return setDate;}
 }
