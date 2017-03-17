7dc24d445d9c7b6a3374735f7acc1dbddc70ef70
Aubry Verret <aubry@awverret.com>
2017-03-17 14:50:17 -0700

    Added route properties that were previously contained in Route class: RouteSetter, RouteColor, RouteWall, SetDate, RouteName.

--- app/src/main/java/com/awverret/gymclimbtracker/model/TopRopeRoute.java
+++ app/src/main/java/com/awverret/gymclimbtracker/model/TopRopeRoute.java
@@ -1,5 +1,7 @@
 package com.awverret.gymclimbtracker.model;
 
+import java.util.Date;
+
 import static java.util.UUID.randomUUID;
 
 /**
@@ -9,20 +11,44 @@ import static java.util.UUID.randomUUID;
 public class TopRopeRoute extends Route {
 
     RopeGrade ropeGrade;
+    RouteColor color;
+    RouteWall wall;
+    String name;
+    RouteSetter setter;
+    Date setDate;
 
     public TopRopeRoute() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
-    public TopRopeRoute(RopeGrade ropeGrade) {
+    public TopRopeRoute(RopeGrade ropeGrade, String name, RouteSetter setter, RouteColor color, RouteWall wall, Date setDate) {
         this.ropeGrade = ropeGrade;
-    }
-
-    public String getId() {
-        return id;
+        this.name = name;
+        this.setter = setter;
+        this.color = color;
+        this.wall = wall;
+        this.setDate = setDate;
     }
 
     public RopeGrade getRopeGrade() {
         return ropeGrade;
     }
+
+    public RouteSetter getSetter() {
+        return setter;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public RouteColor getColor() {
+        return color;
+    }
+
+    public RouteWall getWall() {
+        return wall;
+    }
+
+    public Date getSetDate() {return setDate;}
 }
