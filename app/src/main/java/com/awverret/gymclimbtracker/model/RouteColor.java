e645bf21a5a2b302f96e218ba7b8f8ce0497711d
Aubry Verret <aubry@awverret.com>
2017-03-17 18:31:00 -0700

    Added fromString() method.

--- app/src/main/java/com/awverret/gymclimbtracker/model/RouteColor.java
+++ app/src/main/java/com/awverret/gymclimbtracker/model/RouteColor.java
@@ -25,4 +25,30 @@ public enum RouteColor {
         return this.text;
     }
 
+    public static RouteColor fromString(String text){
+        RouteColor enumValue = null;
+
+        switch(text) {
+            case "Black":  enumValue = BLACK;
+                break;
+            case "White":  enumValue = WHITE;
+                break;
+            case "Tan":  enumValue = TAN;
+                break;
+            case "Orange":  enumValue = ORANGE;
+                break;
+            case "Red":  enumValue = RED;
+                break;
+            case "Purple":  enumValue = PURPLE;
+                break;
+            case "Blue":  enumValue = BLUE;
+                break;
+            case "Green":  enumValue = GREEN;
+                break;
+            case "Yellow":  enumValue = YELLOW;
+                break;
+        }
+        return enumValue;
+    }
+
 }
