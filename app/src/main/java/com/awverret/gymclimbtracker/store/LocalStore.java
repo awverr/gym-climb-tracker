ec07420584e23c250ea28c44bca229625cd211a1
awverr <aubry@awverret.com>
2017-04-11 16:01:22 -0700

    Adding LocalStore interface to project.

---
+++ app/src/main/java/com/awverret/gymclimbtracker/store/LocalStore.java
@@ -0,0 +1,16 @@
+package com.awverret.gymclimbtracker.store;
+
+import com.awverret.gymclimbtracker.model.User;
+import com.google.common.base.Optional;
+
+/**
+ * Created by aubry on 4/11/17.
+ */
+
+public interface LocalStore {
+  //  void clearUser();
+
+    Optional<User> getUser();
+
+    void setUser(User user);
+}
