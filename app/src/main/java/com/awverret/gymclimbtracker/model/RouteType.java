b3464ee3608ef275c479725e4ef201ca91955d2e
awverr <aubry@awverret.com>
2017-06-07 14:40:45 -0700

    Updated to add fromString method.

--- app/src/main/java/com/awverret/gymclimbtracker/model/RouteType.java
+++ app/src/main/java/com/awverret/gymclimbtracker/model/RouteType.java
@@ -6,5 +6,31 @@ package com.awverret.gymclimbtracker.model;
 
 public enum RouteType {
 
-    BOULDER, TOP_ROPE, LEAD
+    BOULDER("Boulder"),
+    TOP_ROPE("Top Rope"),
+    LEAD("Lead");
+
+    private String text;
+
+    RouteType(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public static RouteType fromString(String text){
+        RouteType enumValue = null;
+
+        switch(text) {
+            case "Boulder":  enumValue = BOULDER;
+                break;
+            case "Top Rope":  enumValue = TOP_ROPE;
+                break;
+            case "Lead":  enumValue = LEAD;
+                break;
+        }
+        return enumValue;
+    }
 }
