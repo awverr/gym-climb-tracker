72916f8ab47ed2cba30792c02c3bd7cc8b6ebbdb
awverr <aubry@awverret.com>
2017-06-09 18:42:36 -0700

    Removed routeToStorageRoute methods.

--- app/src/main/java/com/awverret/gymclimbtracker/util/Utils.java
+++ app/src/main/java/com/awverret/gymclimbtracker/util/Utils.java
@@ -1,6 +1,11 @@
 package com.awverret.gymclimbtracker.util;
 
+import com.awverret.gymclimbtracker.model.BoulderRoute;
+import com.awverret.gymclimbtracker.model.LeadRoute;
 import com.awverret.gymclimbtracker.model.RopeGrade;
+import com.awverret.gymclimbtracker.model.Route;
+import com.awverret.gymclimbtracker.model.TopRopeRoute;
+import com.awverret.gymclimbtracker.storage_model.StorageRoute;
 
 /**
  * Created by aubry on 3/8/17.
