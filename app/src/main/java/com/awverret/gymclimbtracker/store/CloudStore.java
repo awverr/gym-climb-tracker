628828dd42167dea14bb9d4239b90fa1fbed19c6
awverr <aubry@awverret.com>
2017-06-09 18:49:10 -0700

    Removed save methods for each individual route type and added a save method for generic route.

--- app/src/main/java/com/awverret/gymclimbtracker/store/CloudStore.java
+++ app/src/main/java/com/awverret/gymclimbtracker/store/CloudStore.java
@@ -2,6 +2,7 @@ package com.awverret.gymclimbtracker.store;
 
 import com.awverret.gymclimbtracker.model.BoulderRoute;
 import com.awverret.gymclimbtracker.model.LeadRoute;
+import com.awverret.gymclimbtracker.model.Route;
 import com.awverret.gymclimbtracker.model.TopRopeRoute;
 import com.awverret.gymclimbtracker.model.User;
 import com.awverret.gymclimbtracker.util.Callback;
@@ -14,9 +15,10 @@ import java.util.List;
 
 public interface CloudStore {
 
-    void saveRoute(LeadRoute route);
-    void saveRoute(TopRopeRoute route);
-    void saveRoute(BoulderRoute route);
+//    void saveRoute(LeadRoute route);
+//    void saveRoute(TopRopeRoute route);
+//    void saveRoute(BoulderRoute route);
+    void saveRoute(Route route);
     void googleLogin(User user);
     void googleLogout();
     List<String> lookUpRoutes(Callback<List<String>> callback);
