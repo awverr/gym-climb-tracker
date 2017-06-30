4d3dd591d1bab1574c2eb7e3b2a6d7b56655f70a
aubry <aubry@awverret.com>
2017-06-30 16:45:14 -0700

    Added createRouteName() method.

--- app/src/main/java/com/awverret/gymclimbtracker/util/Utils.java
+++ app/src/main/java/com/awverret/gymclimbtracker/util/Utils.java
@@ -3,6 +3,7 @@ package com.awverret.gymclimbtracker.util;
 import com.awverret.gymclimbtracker.model.BoulderRoute;
 import com.awverret.gymclimbtracker.model.LeadRoute;
 import com.awverret.gymclimbtracker.model.RopeGrade;
+import com.awverret.gymclimbtracker.model.Route;
 
 /**
  * Created by aubry on 3/8/17.
@@ -12,5 +13,10 @@ public class Utils {
 
     public RopeGrade gradeStringToGradeEnum(String grade){
         return null;
+    }
+
+    public String createRouteName(Route route){
+
+        return route.getWall().toString() + " " + route.getColor() + " " + route.getRouteGrade().toString();
     }
 }
