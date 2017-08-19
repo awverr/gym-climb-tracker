436725fcc71dabeeceb566e86aa0483d117f4830
awverr <aubry@awverret.com>
2017-08-18 18:05:17 -0700

    Adding class to project.

---
+++ app/src/main/java/com/awverret/gymclimbtracker/model/UserRouteHistory.java
@@ -0,0 +1,31 @@
+package com.awverret.gymclimbtracker.model;
+
+import java.util.HashSet;
+
+/**
+ * Created by aubry on 8/18/17.
+ */
+
+public class UserRouteHistory {
+
+    String user;
+    HashSet<Route> userHistory;
+
+    public UserRouteHistory(User user, Route route){
+        this.user = user.getUid();
+        userHistory = new HashSet<>();
+        userHistory.add(route);
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public HashSet<Route> getUserHistory() {
+        return userHistory;
+    }
+
+    public void addRouteToHistory(Route route) {
+        userHistory.add(route);
+    }
+}
