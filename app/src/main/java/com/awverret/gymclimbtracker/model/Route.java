b95bdc0a1a093b96d1615e88bfb44028f88a35ab
aubry <aubry@awverret.com>
2017-09-21 16:45:13 -0700

    Made Route implement Parcelable.

--- app/src/main/java/com/awverret/gymclimbtracker/model/Route.java
+++ app/src/main/java/com/awverret/gymclimbtracker/model/Route.java
@@ -1,5 +1,8 @@
 package com.awverret.gymclimbtracker.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -10,7 +13,7 @@ import static java.util.UUID.randomUUID;
  * Created by aubry on 2/24/17.
  */
 
-public class Route implements Serializable{
+public class Route implements Parcelable{
 
     public RouteType type;
     public String id;
@@ -22,6 +25,15 @@ public class Route implements Serializable{
     //Date setDate;
     public long setDate;
 
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        public Route createFromParcel(Parcel in) {
+            return new Route(in);
+        }
+
+        public Route[] newArray(int size) {
+            return new Route[size];
+        }
+    };
 
     public Route() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
@@ -79,5 +91,34 @@ public class Route implements Serializable{
         return "Route{" +
                 "id='" + id + '\'' +
                 '}';
+    }
+
+    public Route(Parcel in){
+        this.type = (RouteType) in.readSerializable();
+        this.id = in.readString();
+        this.routeGrade = (RouteGrade) in.readSerializable();
+        this.color = (RouteColor) in.readSerializable();
+        this.wall = (RouteWall) in.readSerializable();
+        this.name = in.readString();
+        this.setter = (RouteSetter) in.readSerializable();
+        this.setDate = in.readLong();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+        dest.writeSerializable(this.type);
+        dest.writeString(this.id);
+        dest.writeSerializable(this.routeGrade);
+        dest.writeSerializable(this.color);
+        dest.writeSerializable(this.wall);
+        dest.writeString(this.name);
+        dest.writeSerializable(this.setter);
+        dest.writeLong(this.setDate);
     }
 }
