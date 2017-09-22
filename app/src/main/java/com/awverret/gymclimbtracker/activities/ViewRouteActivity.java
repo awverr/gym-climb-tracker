c32a530546515ce104b3437c9f3e6fa1f5333e17
aubry <aubry@awverret.com>
2017-09-21 17:57:05 -0700

    Initialize route now initializes textView to info from passed in route.

--- app/src/main/java/com/awverret/gymclimbtracker/activities/ViewRouteActivity.java
+++ app/src/main/java/com/awverret/gymclimbtracker/activities/ViewRouteActivity.java
@@ -14,11 +14,15 @@ import com.awverret.gymclimbtracker.model.Route;
 public class ViewRouteActivity extends AppCompatActivity {
 
     TextView routeNameTextView, routeTypeTextView, routeGradeTextView, routeColorTextView, routeWallTextView, routeSetterTextView, routeSetDateTextView;
+    Route route;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_route);
+
+        Bundle data = getIntent().getExtras();
+        route = (Route) data.getParcelable("route");
 
         routeNameTextView = (TextView) findViewById(R.id.route_name_text_view);
         routeTypeTextView = (TextView) findViewById(R.id.route_type_text_view);
@@ -27,9 +31,18 @@ public class ViewRouteActivity extends AppCompatActivity {
         routeWallTextView = (TextView) findViewById(R.id.route_wall_text_view);
         routeSetterTextView = (TextView) findViewById(R.id.route_setter_text_view);
         routeSetDateTextView = (TextView) findViewById(R.id.route_set_date_text_view);
+
+        initializeRoute(route);
     }
 
     public void initializeRoute(Route route){
+        routeNameTextView.setText(route.getName());
+        routeTypeTextView.setText(route.getType().getText());
+        routeGradeTextView.setText(route.getRouteGrade().getText());
+        routeColorTextView.setText(route.getColor().getText());
+        routeWallTextView.setText(route.getWall().getText());
+        routeSetterTextView.setText(route.getSetter().getText());
+        routeSetDateTextView.setText(String.valueOf(route.getSetDate()));
 
     }
 
