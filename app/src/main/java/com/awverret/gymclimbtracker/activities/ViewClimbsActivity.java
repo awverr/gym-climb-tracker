829a998d34894fb9ae5ecfa58e7f8f8f04544d56
awverr <aubry@awverret.com>
2017-10-05 17:05:58 -0700

    Adding to project

---
+++ app/src/main/java/com/awverret/gymclimbtracker/activities/ViewClimbsActivity.java
@@ -0,0 +1,15 @@
+package com.awverret.gymclimbtracker.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.awverret.gymclimbtracker.R;
+
+public class ViewClimbsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_climbs);
+    }
+}
