8f1c55dba35a415b7202a3bdc5e8ad6376fd9c52
aubry <aubry@awverret.com>
2017-11-08 15:29:02 -0800

    Added initializeRecyclerView() method.

--- app/src/main/java/com/awverret/gymclimbtracker/activities/ViewClimbsActivity.java
+++ app/src/main/java/com/awverret/gymclimbtracker/activities/ViewClimbsActivity.java
@@ -2,14 +2,52 @@ package com.awverret.gymclimbtracker.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.awverret.gymclimbtracker.R;
+import com.awverret.gymclimbtracker.model.Climb;
+import com.awverret.gymclimbtracker.store.CloudStore;
+import com.awverret.gymclimbtracker.store.FirebaseCloudStore;
+import com.awverret.gymclimbtracker.util.Callback;
+
+import java.util.ArrayList;
 
 public class ViewClimbsActivity extends AppCompatActivity {
+
+    CloudStore store;
+
+    private RecyclerView mRecyclerView;
+    private ClimbRecyclerAdapter recyclerAdapter;
+
+    ArrayList<Climb> climbList = new ArrayList<>(); //For use in recycler view.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_climbs);
+
+        store = new FirebaseCloudStore(this);
+
+        initializeRecyclerView(this);
+    }
+
+    private void initializeRecyclerView(final ViewClimbsActivity view) {
+        store.lookupClimbs(new Callback<ArrayList<Climb>>() {
+            @Override
+            public void receive(ArrayList<Climb> strings) {
+
+                for(Climb c : strings){
+                    climbList.add(c);
+                }
+                mRecyclerView = (RecyclerView) findViewById(R.id.climb_recycler_view);
+                recyclerAdapter = new ClimbRecyclerAdapter(ViewClimbsActivity.this, climbList);
+                RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+                mRecyclerView.setLayoutManager(mLayoutManager);
+                mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+                mRecyclerView.setAdapter(recyclerAdapter);
+            }
+        });
     }
 }
