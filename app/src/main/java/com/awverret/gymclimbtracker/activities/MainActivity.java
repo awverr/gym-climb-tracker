2afea6d4df50433a57d6c0b2ab10aba346089bef
awverr <aubry@awverret.com>
2018-01-24 13:15:56 -0800

    Set drawer listener.

--- app/src/main/java/com/awverret/gymclimbtracker/activities/MainActivity.java
+++ app/src/main/java/com/awverret/gymclimbtracker/activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -41,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private RecyclerView mRecyclerView;
     private RouteRecyclerAdapter recyclerAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
 
 //    ArrayList<String> routes = new ArrayList<>();
     ArrayList<Route> routeList = new ArrayList<>(); //For use in recylcer view.
@@ -57,6 +59,26 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 //        drawer.setDrawerListener(new DrawerLayout.DrawerListener(this, drawer, toolbar));
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, drawer,
+                R.string.drawer_open, R.string.drawer_close) {
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+            //    getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+         //       getActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        drawer.setDrawerListener(mDrawerToggle);
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
         navigationView.setNavigationItemSelectedListener(this);
