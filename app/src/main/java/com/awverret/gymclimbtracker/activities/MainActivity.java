0218ab3f898a221baf56364550208aea354ceffc
awverr <aubry@awverret.com>
2018-02-01 14:43:25 -0800

    Added onOptionsItemSelected() method.

--- app/src/main/java/com/awverret/gymclimbtracker/activities/MainActivity.java
+++ app/src/main/java/com/awverret/gymclimbtracker/activities/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -150,5 +151,27 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
 
        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_drawer) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
