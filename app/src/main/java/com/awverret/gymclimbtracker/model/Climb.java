3ba8b3f084caea480b8364c88cf82f82db9c5879
aubry <aubry@awverret.com>
2018-04-12 14:59:19 -0700

    Made Climb parcelable.

--- app/src/main/java/com/awverret/gymclimbtracker/model/Climb.java
+++ app/src/main/java/com/awverret/gymclimbtracker/model/Climb.java
@@ -1,17 +1,30 @@
 package com.awverret.gymclimbtracker.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import static java.util.UUID.randomUUID;
 
 /**
  * Created by aubry on 10/3/17.
  */
 
-public class Climb {
+public class Climb implements Parcelable {
 
-    String id;
-    String userId;
-    String routeId;
+    public String id;
+    public String userId;
+    public String routeId;
     //need to add send date
+
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        public Climb createFromParcel(Parcel in) {
+            return new Climb(in);
+        }
+
+        public Climb[] newArray(int size) {
+            return new Climb[size];
+        }
+    };
 
     public Climb(){
 
@@ -33,5 +46,23 @@ public class Climb {
 
     public String getRouteId() {
         return routeId;
+    }
+
+    public Climb(Parcel in){
+        this.id = in.readString();
+        this.userId = in.readString();
+        this.routeId = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.id);
+        dest.writeString(this.userId);
+        dest.writeString(this.routeId);
     }
 }
