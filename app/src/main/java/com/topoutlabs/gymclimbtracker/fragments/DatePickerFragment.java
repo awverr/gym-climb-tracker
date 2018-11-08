de2ea7a03f3d8cb689ea97f9dc127e76060b11d1
aubry <aubry@awverret.com>
2018-11-08 15:59:39 -0800

    Adding bundle to pass date.

--- app/src/main/java/com/topoutlabs/gymclimbtracker/fragments/DatePickerFragment.java
+++ app/src/main/java/com/topoutlabs/gymclimbtracker/fragments/DatePickerFragment.java
@@ -4,7 +4,12 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.DatePicker;
+
+import com.topoutlabs.gymclimbtracker.R;
 
 import java.util.Calendar;
 
@@ -25,5 +30,26 @@ public class DatePickerFragment extends DialogFragment
 
     public void onDateSet(DatePicker view, int year, int month, int day) {
         // Do something with the date chosen by the user
+ //   System.out.println("Date info: " + month + " " + day + " " + year);
+
+    String dateString = String.valueOf(month + 1) + "/" + String.valueOf(day) + "/" + String.valueOf(year);
+
+ //   System.out.println("Date info: " + dateString);
+
+        ViewClimbFragment viewClimbFragment = new ViewClimbFragment();
+
+        Bundle bundle = new Bundle();
+        bundle.putString("date", dateString);
+
+//        viewClimbFragment.setArguments(bundle);
+//
+//        FragmentManager manager = ((AppCompatActivity) context).getSupportFragmentManager();
+//
+//        FragmentTransaction transaction = manager.beginTransaction();
+//
+//        transaction.replace(R.id.fragment_container, viewClimbFragment);
+//        transaction.addToBackStack(null);
+//
+//        transaction.commit();
     }
 }
