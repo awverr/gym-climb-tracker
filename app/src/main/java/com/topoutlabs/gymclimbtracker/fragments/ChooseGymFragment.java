c00a5514d9a66c5b3ba717ad61a8adc7d46afc57
aubry <aubry@awverret.com>
2019-03-20 16:30:40 -0700

    Adding code to onCreateView.

--- app/src/main/java/com/topoutlabs/gymclimbtracker/fragments/ChooseGymFragment.java
+++ app/src/main/java/com/topoutlabs/gymclimbtracker/fragments/ChooseGymFragment.java
@@ -1,16 +1,25 @@
 package com.topoutlabs.gymclimbtracker.fragments;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Spinner;
 
+import com.topoutlabs.gymclimbtracker.R;
 import com.topoutlabs.gymclimbtracker.activities.MainActivity;
 import com.topoutlabs.gymclimbtracker.activities.RouteRecyclerAdapter;
 import com.topoutlabs.gymclimbtracker.model.Route;
 import com.topoutlabs.gymclimbtracker.store.CloudStore;
+import com.topoutlabs.gymclimbtracker.store.FirebaseCloudStore;
+import com.topoutlabs.gymclimbtracker.store.PreferencesLocalStore;
+import com.topoutlabs.gymclimbtracker.util.Callback;
 
 import java.util.ArrayList;
 
@@ -36,6 +45,55 @@ public class ChooseGymFragment extends Fragment implements AdapterView.OnItemSel
         if(activity == null && context instanceof MainActivity){
             activity = (MainActivity) context;
         }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
+        if(view == null) {
+            view = inflater.inflate(R.layout.fragment_choose_gym, container, false);
+
+            gymStateSpinner = (Spinner) view.findViewById(R.id.filter_gym_state_spinner);
+            initializeGymStateSpinner(gymStateSpinner);
+            gymStateSpinner.setOnItemSelectedListener(this);
+
+            store = new FirebaseCloudStore();
+
+            initializeRecyclerView("");
+        }
+
+        return view;
+    }
+
+    private void initializeRecyclerView(final String filterSelection) {
+        gymList.clear();
+        store.lookUpRoutes(new Callback<ArrayList<Route>>() {
+            @Override
+            public void receive(ArrayList<Route> strings) {
+
+                if(!strings.isEmpty()) {
+                    for (Route r : strings) {
+                        if(r.wall.getText().equals(filterSelection) || filterSelection.equals("All States")) {
+                            //            routes.add(r.getName());
+                            gymList.add(r);
+                        }
+                    }
+                }
+
+                //  routeList.stream().filter(S -> S.equals(filterSelection)).collect(Collectors.toList());
+
+                mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+
+                if(localStore.getUser().isPresent()) {
+                    recyclerAdapter = new RouteRecyclerAdapter(routeList, localStore.getUser().get(), activity);
+                }
+
+                RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(activity);
+                mRecyclerView.setLayoutManager(mLayoutManager);
+                mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+                mRecyclerView.setAdapter(recyclerAdapter);
+            }
+        });
     }
 
     @Override
