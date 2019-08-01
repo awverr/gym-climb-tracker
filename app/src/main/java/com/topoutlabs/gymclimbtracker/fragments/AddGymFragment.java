fc4dee66749abff58a2f5600b279aa128e218d62
aubry <aubry@awverret.com>
2019-08-01 14:49:50 -0700

    Completed onClick listener.

--- app/src/main/java/com/topoutlabs/gymclimbtracker/fragments/AddGymFragment.java
+++ app/src/main/java/com/topoutlabs/gymclimbtracker/fragments/AddGymFragment.java
@@ -3,17 +3,22 @@ package com.topoutlabs.gymclimbtracker.fragments;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.topoutlabs.gymclimbtracker.R;
 import com.topoutlabs.gymclimbtracker.activities.MainActivity;
+import com.topoutlabs.gymclimbtracker.model.Gym;
 import com.topoutlabs.gymclimbtracker.store.CloudStore;
+import com.topoutlabs.gymclimbtracker.store.FirebaseCloudStore;
 
 public class AddGymFragment extends Fragment implements AdapterView.OnItemSelectedListener{
 
@@ -51,25 +56,22 @@ public class AddGymFragment extends Fragment implements AdapterView.OnItemSelect
             saveGymButton = view.findViewById(R.id.save_gym_button);
             saveGymButton.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
-                    String stringType = (String) routeTypeSpinner.getSelectedItem();
-                    String stringGrade = (String) routeGradeSpinner.getSelectedItem();
-                    String stringColor = (String) routeColorSpinner.getSelectedItem();
-                    String stringWall = (String) routeWallSpinner.getSelectedItem();
-                    String stringSetter = editTextSetter.getText().toString();
-                    RouteColor color = RouteColor.fromString(stringColor);
-                    RouteWall wall = RouteWall.fromString(stringWall);
 
-                    store.saveRoute(new Route(type, activity.getLocalStore().getGym().getId(), grade, null, stringSetter, color, wall, dateInMillis));
+                    EditText editTextName = (EditText) view.findViewById(R.id.gym_name_text_view);
+                    String stringGymState = (String) gymStateSpinner.getSelectedItem();
+                    String stringName = editTextName.getText().toString();
 
-                    // Now return to the viewAllRoutesFragment
-                    ViewAllRoutesFragment viewAllRoutesFragment = new ViewAllRoutesFragment();
+                    store.saveGym(new Gym(stringName, stringGymState));
 
-                    viewAllRoutesFragment.setArguments(getActivity().getIntent().getExtras());
-
-                    FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
-
-                    transaction.replace(R.id.fragment_container, viewAllRoutesFragment).commit();
-                    transaction.addToBackStack(null);
+                    // TODO: Which fragment do you want to return to?
+//                    ViewAllRoutesFragment viewAllRoutesFragment = new ViewAllRoutesFragment();
+//
+//                    viewAllRoutesFragment.setArguments(getActivity().getIntent().getExtras());
+//
+//                    FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+//
+//                    transaction.replace(R.id.fragment_container, viewAllRoutesFragment).commit();
+//                    transaction.addToBackStack(null);
                 }
             });
 
@@ -77,6 +79,15 @@ public class AddGymFragment extends Fragment implements AdapterView.OnItemSelect
         }
 
         return view;
+    }
+
+    public void initializeGymStateSpinner(Spinner spinner){
+        //route_type_spinner
+
+        ArrayAdapter<CharSequence> routeTypeAdapter = ArrayAdapter.createFromResource(activity,
+                R.array.route_type_array, android.R.layout.simple_spinner_item);
+        routeTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(routeTypeAdapter);
     }
 
     @Override
